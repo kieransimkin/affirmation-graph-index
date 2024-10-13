7c46c172687cc777991b18bacf572d6c3651a021
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2024-10-13 15:26:46 +0100

    Update oura.js

--- routes/oura.js
+++ routes/oura.js
@@ -6,11 +6,13 @@ router.post('/', function(req, res, next) {
   console.log(req.body);
   for (var output of req.body.transaction.outputs) { 
     var address = cardano.Cardano.Address.fromBech32(output.address);
+    console.log(address);
     var stakeKey = address.getStakeCredential();
+    console.log(stakeKey);
     var stakeAddress = cardano.Cardano.RewardAddress.fromCredentials(address.getNetworkId(), stakeKey);
     console.log(stakeAddress);
     console.log(output);
-    
+
   }
   
   res.send('respond with a resource');
