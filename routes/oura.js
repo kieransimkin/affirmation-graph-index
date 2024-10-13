e65962afbe89febb84f00a57017b0b9e500dc1ff
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2024-10-13 15:12:01 +0100

    Fixes

--- routes/oura.js
+++ routes/oura.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var cardano = require('@cardano-sdk/core')
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   console.log(req.body);
+  for (var output of req.body.transaction.outputs) { 
+    cardano.Cardano.Address.fromBech32(output.address)
+  }
+  
   res.send('respond with a resource');
 });
 
